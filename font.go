09699aa36e901d0f78f99f0dd30b0845ca1476d7
Daniel <danielallendeutsch@gmail.com>
2016-10-13 11:16:00 -0500

    breaks out Font to seperate file and improves parsing logic.

---
+++ font.go
@@ -0,0 +1,55 @@
+package figure
+
+import (
+	"fmt"
+  "os"
+  "bufio"
+  "strconv"
+  "regexp"
+)
+
+type Font struct {
+  Letters [][]string
+  Height int
+}
+
+func NewFont(name string) Font {
+  var fnt Font
+
+  file_path := fmt.Sprintf("../figure/fonts/%s.flf", name)
+  file, _ := os.Open(file_path)
+  defer file.Close()
+  scanner := bufio.NewScanner(file)
+
+  fnt.Letters = append(fnt.Letters, []string{})
+
+  counter := 0
+  for scanner.Scan() {
+    text := scanner.Text()
+    if fnt.Height == 0 && text[:4] == "flf2" {
+      r, _ := regexp.Compile(`\d`)
+      matches := r. FindAllString(text, -1)
+      fnt.Height, _ = strconv.Atoi(matches[1])
+    }
+
+
+    fnt.Letters = append(fnt.Letters, []string{})
+    cutLength := 1
+
+    if counter > 0 {
+      if len(text) > 2 && text[len(text)-2:] == "@@" {
+        cutLength = 2
+      }
+      fnt.Letters[counter] = append(fnt.Letters[counter], text[:len(text)-cutLength])
+    }
+
+    if len(text) > 2 && text[len(text)-2:] == "@@" {
+      counter ++
+    }
+  }
+  for i := 0 ; i < fnt.Height ; i ++ {
+    fnt.Letters[0] = append(fnt.Letters[0], "   ")
+  }
+
+  return fnt
+}
--- go-figure.go
+++ go-figure.go
@@ -3,69 +3,28 @@ package figure
 import (
 	"fmt"
 	"log"
-  "os"
-  "bufio"
-  "strconv"
 )
 
 type Figure struct {
   Phrase string
-  Font string
+  Font Font
 }
 
-type Font struct {
-  Letters [][]string
-  Height int
-}
-
-func NewFont(name string) Font {
-  var fnt Font
-
-  file, _ := os.Open("/Users/Daniel/Documents/go-workspace/src/go-figure/fonts/alphabet.flf")
-  defer file.Close()
-  scanner := bufio.NewScanner(file)
-
-  fnt.Letters = append(fnt.Letters, []string{})
-
-  counter := 0
-  for scanner.Scan() {
-    text := scanner.Text()
-    if fnt.Height == 0 && text[:4] == "flf2" {
-      fnt.Height, _ = strconv.Atoi(string(text[5]))
-    }
-
-
-    fnt.Letters = append(fnt.Letters, []string{})
-    cutLength := 1
-
-    if counter > 0 {
-      if text[len(text)-2:] == "@@" {
-        cutLength = 2
-      }
-      fnt.Letters[counter] = append(fnt.Letters[counter], text[:len(text)-cutLength])
-    }
-
-    if text == "@@@@@@@@" {
-      counter = 1
-    } else if len(text) > 2 && text[len(text)-2:] == "@@" {
-      counter += 1
-    }
+func NewFigure(phrase string, fontName string) Figure {
+  if fontName == "" {
+    fontName = "alphabet"
   }
-  for i := 0 ; i < fnt.Height ; i ++ {
-    fnt.Letters[0] = append(fnt.Letters[0], "   ")
-  }
-
-  return fnt
+  return Figure{ phrase, NewFont(fontName) }
 }
 
-func (f Figure) Println(fnt Font) {
-  for r := 0 ; r < fnt.Height ; r++ {
+func (fig Figure) Print() {
+  for r := 0 ; r < fig.Font.Height ; r++ {
     printRow := ""
-    for c := 0 ; c < len(f.Phrase) ; c++ {
-      char := f.Phrase[c]
-      fontIndex := char - 32
-      if true {
-        printRow = printRow + fnt.Letters[fontIndex][r]
+    for c := 0 ; c < len(fig.Phrase) ; c++ {
+      char := fig.Phrase[c]
+      if char >= ' ' && char <= '~' {
+        fontIndex := char - 32
+        printRow = printRow + fig.Font.Letters[fontIndex][r]
       } else {
 				log.Fatal("invalid input.")
       }
