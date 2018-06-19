8babb42a02cfe7735e569205375941d19b7d46e0
Zack Scholl <zack.scholl@gmail.com>
2018-06-18 19:09:14 -0700

    Add test for default

--- figure_test.go
+++ figure_test.go
@@ -14,3 +14,14 @@ func ExampleAlphabet() {
 	// H  H e e l l o o   w w w o o r   l d  d
 	// H  H ee  l l ooo    w w  ooo r   l  ddd
 }
+
+func ExampleDefault() {
+	myFigure := NewFigure("Hello world", "", true)
+	fmt.Println(myFigure)
+	// Output:
+	// _   _          _   _                                       _       _
+	//  | | | |   ___  | | | |   ___     __      __   ___    _ __  | |   __| |
+	//  | |_| |  / _ \ | | | |  / _ \    \ \ /\ / /  / _ \  | '__| | |  / _` |
+	//  |  _  | |  __/ | | | | | (_) |    \ V  V /  | (_) | | |    | | | (_| |
+	//  |_| |_|  \___| |_| |_|  \___/      \_/\_/    \___/  |_|    |_|  \__,_|
+}
